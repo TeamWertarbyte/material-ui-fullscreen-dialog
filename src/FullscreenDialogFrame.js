0befaeee8ffbd0d9af18c64351d7d7c54809fad4
Minivera <guillaume.stpierre@carleton.ca>
2018-04-27 08:55:55 -0400

    Migrated to the v2 API of react-transition-group (#16)
    * Migrated to the v2 API of react-transition-group
    
    * Fixed small CSS issue

--- src/FullscreenDialogFrame.js
+++ src/FullscreenDialogFrame.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import TransitionGroup from 'react-transition-group/TransitionGroup'
+import Transition from 'react-transition-group/Transition'
 import AutoLockScrolling from 'material-ui/internal/AutoLockScrolling'
 
 const styles = {
@@ -12,95 +12,52 @@ const styles = {
     left: 0,
     zIndex: 1500,
     background: '#fafafa'
-  }
-}
-
-class TransitionItem extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      style: {
-        opacity: 0,
-        transition: 'all 225ms cubic-bezier(0.0, 0.0, 0.2, 1)',
-        transform: 'translate(0, 56px)'
-      }
+  },
+  transition: {
+    entering: {
+      opacity: 0,
+      transition: 'all 225ms cubic-bezier(0.0, 0.0, 0.2, 1)',
+      transform: 'translate(0, 56px)'
+    },
+    entered: {
+      opacity: 1,
+      transition: 'all 225ms cubic-bezier(0.0, 0.0, 0.2, 1)',
+      transform: 'translate(0, 0px)'
+    },
+    exiting: {
+      opacity: 0,
+      transition: 'all 195ms cubic-bezier(0.4, 0.0, 1, 1)',
+      transform: 'translate(0, 56px)'
+    },
+    exited: {
+      opacity: 0,
+      display: 'none',
+      transition: 'all 225ms cubic-bezier(0.0, 0.0, 0.2, 1)',
+      transform: 'translate(0, 56px)'
     }
   }
-
-  componentWillUnmount () {
-    clearTimeout(this.enterTimeout)
-    clearTimeout(this.leaveTimeout)
-  }
-
-  componentWillEnter (callback) {
-    this.componentWillAppear(callback)
-  }
-
-  componentWillAppear (callback) {
-    this.enterTimeout = setTimeout(() => {
-      this.setState({
-        style: {
-          opacity: 1,
-          transition: 'all 225ms cubic-bezier(0.0, 0.0, 0.2, 1)',
-          transform: 'translate(0, 0px)'
-        }
-      })
-      this.enterTimeout = setTimeout(callback, 225) // matches transition duration
-    })
-  }
-
-  componentWillLeave (callback) {
-    this.setState({
-      style: {
-        opacity: 0,
-        transition: 'all 195ms cubic-bezier(0.4, 0.0, 1, 1)',
-        transform: 'translate(0, 56px)'
-      }
-    })
-
-    this.leaveTimeout = setTimeout(callback, 195) // matches transition duration
-  }
-
-  render () {
-    const {
-      style,
-      children,
-      ...other
-    } = this.props
-
-    return (
-      <div {...other} style={{ ...style, ...this.state.style }}>
-        {children}
-      </div>
-    )
-  }
-}
-
-TransitionItem.propTypes = {
-  children: PropTypes.node,
-  style: PropTypes.object
 }
 
 export default function FullscreenDialogFrame ({ children, open, style }) {
   return (
-    <TransitionGroup
+    <Transition
+      in={open}
+      timeout={{exit: 225, enter: 225}}
       component='div'
-      transitionAppear
-      transitionAppearTimeout={225}
-      transitionEnter
-      transitionEnterTimeout={225}
+      appear
+      enter
     >
-      {open && (
-        <TransitionItem style={{
+      {(state) => (
+        <div style={{
+          ...style,
           ...styles.root,
-          ...style
-        }}
-        >
+          ...styles.transition[state]
+        }}>
           {children}
-        </TransitionItem>
+          <AutoLockScrolling lock={open} />
+        </div>
       )}
-      <AutoLockScrolling lock={open} />
-    </TransitionGroup>
+    </Transition>
   )
 }
 
