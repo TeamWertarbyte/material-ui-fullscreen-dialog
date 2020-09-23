41d5d14d08de70741e72644ffd977418dfc31a97
Maik Marschner <m.marschner@wertarbyte.com>
2020-09-23 15:05:00 +0200

    Add typings for the FullscreenDialogFrame.

---
+++ src/FullscreenDialogFrame.d.ts
@@ -0,0 +1,7 @@
+import { DialogProps } from "@material-ui/core";
+
+export interface FullscreenDialogFrameProps extends DialogProps {}
+
+export default function FullscreenDialogFrame(
+  props: FullscreenDialogFrameProps
+): JSX.Element;
